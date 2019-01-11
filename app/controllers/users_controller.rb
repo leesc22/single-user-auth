53fa0f4e6e3ad25824266fcd076f5bbd995f7504
leesc <leesiewching22@gmail.com>
2019-01-11 09:16:41 +0800

    Only one user can be registered

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user   , only: [:show, :edit, :update, :destroy]
+  before_action :single_user, only: [:new, :create]
 
   # GET /users
   # GET /users.json
@@ -70,5 +71,9 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+    def single_user
+      redirect_to(root_url) if User.exists?
     end
 end
